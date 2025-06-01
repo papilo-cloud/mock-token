6b74ab2a14c51ca79cdbe8d46675e4fb585e41ad
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-01 20:52:56 +0100

    added the required plugins to verify the transaction on sepolia etherscan also addded the required keys from wirh dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,26 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-chai-matchers")
-require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require('dotenv').config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
+  }
 };
