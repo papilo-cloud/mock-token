f81abe58b30468fc1d2d070be6153289975bf6b4
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-01 17:59:18 +0100

    hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-chai-matchers")
+require("@nomiclabs/hardhat-ethers")
+
+module.exports = {
+  solidity: "0.8.28",
+};
