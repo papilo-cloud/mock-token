2dd74f2952976747718dc4d9fb11dc0bb8a64b76
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-03 12:28:12 +0100

    signed messages

--- ethersjs/signMessage.js
+++ ethersjs/signMessage.js
@@ -2,17 +2,15 @@ const {ethers, providers, Wallet} = require('ethers')
 require('dotenv').config()
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const DEPLOYED_ADDRESS = process.env.DEPLOYED_ADDRESS;
-const ADDRESS = process.env.ADDRESS;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const RECIPIENT_ADDRESS = process.env.RECEIVERS_ADDRESS;
 
 const signMessage = async () => {
     const provider = new providers.JsonRpcProvider(SEPOLIA_RPC_URL);
 
     const wallet = new Wallet(PRIVATE_KEY, provider);
 
-    // const output = await;
+    const msg = 'ethers.js is a powerful library';
+    const output = await wallet.signMessage(msg);
     console.log('Signature of message:', output);
 }
 
